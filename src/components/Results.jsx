8b4132a96bef527f9d4f6fb2c7ba5345c0ab5e4f
Sterling Newsom <sterlingnewsom808@gmail.com>
2023-06-06 21:25:34 -0400

    made some changes to result card

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Results = () => {
 return (
     <div className ="result-card">
-        <h4 className="color-lgt">Your Result</h4>
+        <h4 className="color-lgt result-card-title">Your Result</h4>
         <div className ="result-circle">
         <h1 id ="result-num">76</h1>
         <p className="color-lgt">of 100</p>
         </div>
         <h3>Great</h3>
-        <p className="color-lgt">Your performance exceed 65% of the people conducting the test test here!</p>
+        <p className="color-lgt your-performance">Your performance exceed 65% of the people conducting the test test here!</p>
     </div>
 )
 }
